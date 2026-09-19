ec33de672f6fdc15c21d9d761057a52f0942709e
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2026-09-20 02:50:22 +0300

    Register Zurb in the template registry like Mailchimp

--- src/rendering/template-registry.ts
+++ src/rendering/template-registry.ts
@@ -3,12 +3,15 @@ import type { Validator } from '../validation/validator.js';
 import { HackernoonRenderer } from '../templates/hackernoon/hackernoon.renderer.js';
 import { NomoretogoRenderer } from '../templates/nomoretogo/nomoretogo.renderer.js';
 import { MailchimpRenderer } from '../templates/mailchimp/mailchimp.renderer.js';
+import { ZurbRenderer } from '../templates/zurb/zurb.renderer.js';
 import { HackernoonValidator } from '../validation/hackernoon.validator.js';
 import { NomoretogoValidator } from '../validation/nomoretogo.validator.js';
 import { MailchimpValidator } from '../validation/mailchimp.validator.js';
+import { ZurbValidator } from '../validation/zurb.validator.js';
 import { hackernoonData } from '../scripts/content/hackernoon-data.js';
 import { nomoretogoData } from '../scripts/content/nomoretogo-data.js';
 import { mailchimpData } from '../scripts/content/mailchimp-data.js';
+import { zurbData } from '../scripts/content/zurb-data.js';
 import { ConfigError } from '../errors/index.js';
 
 export interface TemplateEntry<T> {
@@ -32,6 +35,11 @@ export const templateRegistry = {
     renderer: new MailchimpRenderer(),
     validator: new MailchimpValidator(),
     sampleData: mailchimpData,
+  },
+  zurb: {
+    renderer: new ZurbRenderer(),
+    validator: new ZurbValidator(),
+    sampleData: zurbData,
   },
 } as const;
 
